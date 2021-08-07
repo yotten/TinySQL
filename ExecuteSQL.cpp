e3aa039e37d749a02b424ac83a8b915f77ed65e2
Akihiro Yoshida <yakihiro@ac.auone-net.jp>
2021-08-08 00:02:56 +0900

    006.Operatorをclass化
    [----------] Global test environment tear-down
    [==========] 215 tests from 1 test case ran. (5309 ms total)
    [  PASSED  ] 215 tests.

--- ExecuteSQL.cpp
+++ ExecuteSQL.cpp
@@ -95,11 +95,11 @@ public:
 };
 
 //! WHERE句に指定する演算子の情報を表します。
-typedef struct
-{
+class Operator {
+public:
 	TokenKind kind; //!< 演算子の種類を、演算子を記述するトークンの種類で表します。
 	int order; //!< 演算子の優先順位です。
-} Operator;
+};
 
 //! トークンを表します。
 typedef struct
