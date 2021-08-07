1a0fcb47131e4f0ca3644aa89b6dcab5a303635b
Akihiro Yoshida <yakihiro@ac.auone-net.jp>
2021-08-08 00:11:39 +0900

    009.ExtensionTreeNodeをclass化
    [----------] Global test environment tear-down
    [==========] 215 tests from 1 test case ran. (5285 ms total)
    [  PASSED  ] 215 tests.

--- ExecuteSQL.cpp
+++ ExecuteSQL.cpp
@@ -116,19 +116,19 @@ public:
 };
 
 //! WHERE句の条件の式木を表します。
-typedef struct _extension_tree_node
-{
-	struct _extension_tree_node *parent; //!< 親となるノードです。根の式木の場合はNULLとなります。
-	struct _extension_tree_node *left;   //!< 左の子となるノードです。自身が末端の葉となる式木の場合はNULLとなります。
+class ExtensionTreeNode {
+public:
+	struct ExtensionTreeNode *parent; //!< 親となるノードです。根の式木の場合はNULLとなります。
+	struct ExtensionTreeNode *left;   //!< 左の子となるノードです。自身が末端の葉となる式木の場合はNULLとなります。
 	Operator middleOperator;             //!< 中置される演算子です。自身が末端のとなる式木の場合の種類はNOT_TOKENとなります。
-	struct _extension_tree_node *right;  //!< 右の子となるノードです。自身が末端の葉となる式木の場合はNULLとなります。
+	struct ExtensionTreeNode *right;  //!< 右の子となるノードです。自身が末端の葉となる式木の場合はNULLとなります。
 	bool inParen;                        //!< 自身がかっこにくるまれているかどうかです。
 	int parenOpenBeforeClose;            //!< 木の構築中に0以外となり、自身の左にあり、まだ閉じてないカッコの開始の数となります。
 	int signCoefficient;                 //!< 自身が葉にあり、マイナス単項演算子がついている場合は-1、それ以外は1となります。
 	Column column;                       //!< 列場指定されている場合に、その列を表します。列指定ではない場合はcolumnNameが空文字列となります。
 	bool calculated;                     //!< 式の値を計算中に、計算済みかどうかです。
 	Data value;                          //!< 指定された、もしくは計算された値です。
-} ExtensionTreeNode;
+};
 
 //! 行の情報を入力のテーブルインデックス、列インデックスの形で持ちます。
 typedef struct
